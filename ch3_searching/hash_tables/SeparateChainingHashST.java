68a7329614e35fea6fa3ef4abf12bf7015ae52ed
shy_away <149800599+shy-away@users.noreply.github.com>
2026-05-19 07:02:07 -0500

    chore: SeparateChainingHashST prime resizing

--- ch3_searching/hash_tables/SeparateChainingHashST.java
+++ ch3_searching/hash_tables/SeparateChainingHashST.java
@@ -5,26 +5,71 @@ import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class SeparateChainingHashST<Key, Value> {
-  private int N, M;
+  private int N, M, lgM;
+  private static int defaultSize = 997;
+  private double probeTolerance;
+  private static double defaultProbeTolerance = 5.0;
   private SequentialSearchST<Key, Value>[] chains;
 
+  private static int[] primes = new int[] {
+      0, 0, 0, 0, 0,
+      31, 61, 127, 251, 509, 1021, 2039, 4093, 8191, 16381, 32749, 65521, 131071, 262139, 524287, 1048573, 2097143,
+      4194301, 8388593, 16777213, 33554393, 67108859, 134217689, 268435399, 536870909, 1073741789, 2147483647
+  };
+
   public SeparateChainingHashST() {
-    this(997);
+    this(defaultSize, defaultProbeTolerance);
+  }
+
+  public SeparateChainingHashST(double probeTolerance) {
+    this(defaultSize, probeTolerance);
+  }
+
+  public SeparateChainingHashST(int M) {
+    this(M, defaultProbeTolerance);
   }
 
   @SuppressWarnings("unchecked")
-  public SeparateChainingHashST(int M) {
+  public SeparateChainingHashST(int M, double probeTolerance) {
     this.M = M;
+    this.probeTolerance = probeTolerance;
     chains = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
     for (int i = 0; i < M; i++) {
       chains[i] = new SequentialSearchST<>();
     }
+    setLgM();
   }
 
   private int hash(Key key) {
     // mask off sign bit to prevent negative remainders
-    return (key.hashCode() & 0x7fffffff) % M;
-    // Note: Math.abs() does not work for -2^31. Math.abs(-2147483648) == -2147483648
+    int t = key.hashCode() & 0x7fffffff;
+    // Note: Math.abs() does not work for -2^31. Math.abs(-2147483648) ==
+    // -2147483648
+
+    if (lgM < 26)
+      t = t % primes[lgM + 5];
+
+    return t % M;
+  }
+
+  private void setLgM() {
+    lgM = (int) (Math.log(M) / Math.log(2));
+  }
+
+  private void resize(int newSize) {
+    SeparateChainingHashST<Key, Value> t = new SeparateChainingHashST<>(newSize, probeTolerance);
+
+    for (int i = 0; i < chains.length; i++) {
+      SequentialSearchST<Key, Value> currentChain = chains[i];
+
+      for (Key key : currentChain.keys()) {
+        t.put(key, currentChain.get(key));
+      }
+    }
+
+    chains = t.chains;
+    M = t.M;
+    setLgM();
   }
 
   public int size() {
@@ -43,6 +88,9 @@ public class SeparateChainingHashST<Key, Value> {
     if (val == null)
       delete(key);
 
+    if (N >= M / 2)
+      resize(2 * M);
+
     chains[hash(key)].put(key, val);
     N++;
   }
@@ -53,6 +101,9 @@ public class SeparateChainingHashST<Key, Value> {
 
     chains[hash(key)].delete(key);
     N--;
+
+    if (N > 0 && N <= M / 8)
+      resize(M / 2);
   }
 
   public Iterable<Key> keys() {
